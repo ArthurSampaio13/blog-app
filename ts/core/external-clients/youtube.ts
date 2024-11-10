591baae585c6510d87b5f764aefa9506c480393a
ArthurSampaio13 <arthur.sampaio@upe.br>
2024-11-10 10:33:09 -0300

    fix: erro no dict de cookies

--- ts/core/external-clients/youtube.ts
+++ ts/core/external-clients/youtube.ts
@@ -3,7 +3,7 @@ import ytdl from '@distube/ytdl-core';
 
 class YouTubeClient {
     async downloadAudio(url: string, filepath: string): Promise<void> {
-        const cookies = [];
+        const cookies : { name: string; value: string }[] = [];
 
         const cookieKeys = [ 'HSID', 'SSID', 'SID' ];
         for (const key of cookieKeys) {
