8459ff3b41020a1a813d78851b1c0a9badda1cf1
Arthur <arthur.sampaio@upe.br>
2024-11-20 10:52:30 -0300

    feat: script dev

---
+++ ts/dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm run blog-ui-ts & \
+npm run blog-ui-node & \
+npm run blog-api-node & \
+npm run blog-audio & \
+npm run blog-ai-transcribe & \
+npm run blog-ai-create-post & \
+npm run blog-ui-tailwind && fg
